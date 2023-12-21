7151f537bf11cc91bdc25f0c36578b720bf89212
scizorman <tueda1207@gmail.com>
2023-12-21 17:21:32 +0900

    Add the TDD exercise of 'Stack'

--- src/main/kotlin/clean/craftsmanship/stack/Stack.kt
+++ src/main/kotlin/clean/craftsmanship/stack/Stack.kt
@@ -1,3 +1,21 @@
 package clean.craftsmanship.stack
 
-class Stack
+class Stack {
+    private var size = 0
+    private var elements: MutableList<Int> = mutableListOf()
+
+    fun isEmpty() = size == 0
+
+    fun push(element: Int) {
+        elements.add(size++, element)
+    }
+
+    fun pop(): Int {
+        if (size == 0) throw Underflow()
+        return elements[--size]
+    }
+
+    fun getSize() = size
+}
+
+class Underflow : RuntimeException()
--- src/test/kotlin/clean/craftsmanship/stack/StackTest.kt
+++ src/test/kotlin/clean/craftsmanship/stack/StackTest.kt
@@ -1,10 +1,60 @@
 package clean.craftsmanship.stack
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class StackTest {
+    private val stack = Stack()
+
     @Test
     fun canCreateStack() {
-        Stack()
+        assertTrue(stack.isEmpty())
+    }
+
+    @Test
+    fun `stack is not empty when after one push`() {
+        stack.push(0)
+        assertFalse(stack.isEmpty())
+        assertEquals(1, stack.getSize())
+    }
+
+    @Test
+    fun `stack is empty when after one push and one pop`() {
+        stack.push(0)
+        stack.pop()
+        assertTrue(stack.isEmpty())
+    }
+
+    @Test
+    fun `stack size is two when after two pushes`() {
+        stack.push(0)
+        stack.push(0)
+        assertEquals(2, stack.getSize())
+    }
+
+    @Test
+    fun `throws underflow when popping empty stack`() {
+        assertThrows<Underflow> {
+            stack.pop()
+        }
+    }
+
+    @Test
+    fun `pop x after pushing x`() {
+        stack.push(99)
+        assertEquals(99, stack.pop())
+        stack.push(88)
+        assertEquals(88, stack.pop())
+    }
+
+    @Test
+    fun `should pop y then x after pushing x and y`() {
+        stack.push(99)
+        stack.push(88)
+        assertEquals(88, stack.pop())
+        assertEquals(99, stack.pop())
     }
 }
